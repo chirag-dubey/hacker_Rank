178fece198eaec66dd3682686b871f742cf9adb8
chirag-dubey <chiragdubey9818@gmail.com>
2025-02-14 21:44:36 +0530

    Add files via upload

---
+++ Bitwise_Set i-th bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int n;
+    
+    scanf("%d",&n);
+       int i;
+    scanf("%d",&i);
+    
+        a=(n|1<<i);
+           printf("%d",a);
+
+
+    return 0;
+}
