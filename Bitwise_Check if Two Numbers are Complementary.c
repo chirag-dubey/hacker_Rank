f3d05d4ac98780b7de3c243674bc4c5e89b72762
chirag-dubey <chiragdubey9818@gmail.com>
2025-02-21 23:53:19 +0530

    Create Bitwise_Check if Two Numbers are Complementary.c

---
+++ Bitwise_Check if Two Numbers are Complementary.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+         int a; 
+    scanf("%d",&a);
+         int b; 
+    scanf("%d",&b);
+    
+    if(a==(~b)){
+        printf("YES");
+    } 
+    else{
+        printf("NO");
+    }
+    
+
+    
+    return 0;
+}
