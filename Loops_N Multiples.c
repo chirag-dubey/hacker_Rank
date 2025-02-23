892da389e2c2cff14aaedf03e8c6ffd9f7ebb078
chirag-dubey <chiragdubey9818@gmail.com>
2025-02-23 23:51:58 +0530

    Create Loops_N Multiples.c

---
+++ Loops_N Multiples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+         int n;
+        scanf("%d",&n);
+          
+    for(int i=1;i<=n;++i){
+  int m;
+        scanf("%d",&m);
+        
+        printf("%d ",i*m);
+    }
+    return 0;
+}
